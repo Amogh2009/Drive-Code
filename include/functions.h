5b36da6d3d4b7c3aae1b990cd0070da00e2ccdc2
Amogh2009 <88294952+Amogh2009@users.noreply.github.com>
2021-10-25 16:03:54 -0700

    Add files via upload

---
+++ include/functions.h
@@ -0,0 +1,44 @@
+#include "main.h"
+#include "motorSetup.h"
+
+
+//Useful Constants
+const double wheelCircumfrence = 2.75 * M_PI;
+
+//For debugging things
+void printOnScreen(){
+	//lcd::print(1, "Velocity FL: %f", FrontLeft.get_actual_velocity());
+	//lcd::print(2, "Target Velocity FL: %f", drive.wheelTL);
+  lcd::print(0, "Inertial Reading: %f", inertial.get_rotation());
+  lcd::print(1, "Y Wheel Reading: %f", ((double) yWheel.get_value()));
+  lcd::print(2, "X Wheel Reading: %f", ((double) xWheel.get_value()));
+}
+
+void driverControl(double l, double r){
+  //Calculates speed of wheels for driver control
+
+	FrontLeft.move_velocity(l);
+	FrontRight.move_velocity(r);
+	BackLeft.move_velocity(l);
+	BackRight.move_velocity(r);
+}
+
+
+void stopDrive(bool hold = false){
+  //Shortcut to stop the drive quickly
+  if(hold){
+    FrontLeft.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    FrontRight.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    BackLeft.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    BackRight.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+  }
+  FrontLeft.move_velocity(0);
+	FrontRight.move_velocity(0);
+	BackLeft.move_velocity(0);
+	BackRight.move_velocity(0);
+  delay(100);
+  FrontLeft.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+  FrontRight.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+  BackLeft.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+  BackRight.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+}
---
+++ include/motorSetup.h
@@ -0,0 +1,60 @@
+#include "main.h"
+
+
+using namespace pros;
+
+const double ticksPerDeg = 900/360;
+//200*4.125*pi/60 * sqrt(2), = 60
+const double robotSpeed = 43.196 * sqrt(2);
+const double rotationSpeed = 200;
+
+extern Controller control;
+extern Motor FrontLeft;
+extern Motor FrontRight;
+extern Motor BackLeft;
+extern Motor BackRight;
+extern Motor LeftIntake;
+extern Motor RightIntake;
+
+//Ports
+const int FLPort = 3;
+const int FRPort = 4;
+const int BLPort = 5;
+const int BRPort = 6;
+const int LIPort = 8;
+const int RIPort = 5;
+const int LRPort = 2;
+const int RRPort = 3;
+const int IMUPort = 1;
+
+Controller control (E_CONTROLLER_MASTER);
+Motor FrontLeft(FLPort, true);
+Motor FrontRight(FRPort, true);
+Motor BackLeft(BLPort, true);
+Motor BackRight(BRPort, false);
+Motor LeftIntake(LIPort, false);
+Motor RightIntake(RIPort, true);
+Motor BackRoller(LRPort, true);
+Motor MainRoller(RRPort, true);
+Imu inertial(IMUPort);
+
+ADIEncoder yWheel('C', 'D', true);
+ADIEncoder xWheel('A', 'B', false);
+
+
+//Calibrate the sensors
+void calibrateSensors(){
+  xWheel.reset();
+  yWheel.reset();
+  inertial.reset();
+
+  int timeInit = millis();
+
+  inertial.reset();
+  while(inertial.is_calibrating()){
+    lcd::print(1, "Calibrating");
+    delay(10);
+  }
+  delay(2000);
+  lcd::print(1, "Calibration took %f", millis() - timeInit);
+}
