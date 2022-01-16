ec963fe0828d0aed20595419c9a527c6757da4d3
Amogh2009 <88294952+Amogh2009@users.noreply.github.com>
2022-01-16 14:51:42 -0800

    Update motorSetup.h

--- include/motorSetup.h
+++ include/motorSetup.h
@@ -21,8 +21,8 @@ const int FLPort = 3;
 const int FRPort = 5;
 const int BLPort = 14;
 const int BRPort = 16;
-const int MRPort = 17;
 const int MLPort = 10;
+const int MRPort = 17;
 
 const int BCPort = 7;
 const int FCPort = 12;
